13d0ffec8456c7ed3f97a4a8fa59ac6e22c5a98a
Violet Wolf <violetchandon@gmail.com>
2022-10-18 15:24:47 -0700

    Setup function to grab random url

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
+import memesData from '../memesData';
+
 const Meme = () => {
+  const getMemeImage = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  };
+
   return (
     <main>
-      <form className='form' action='submit'>
+      <div className='form'>
         <input className='form-input' type='text' placeholder='top text' />
         <input className='form-input' type='text' placeholder='bottom text' />
-        <button className='form-button'>Get a new meme image</button>
-      </form>
+        <button className='form-button' onClick={getMemeImage}>
+          Get a new meme image
+        </button>
+      </div>
     </main>
   );
 };
