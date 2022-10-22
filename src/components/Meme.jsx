ea071437394ebfa5a2d334a35c717b7ffe0a5bdf
Violet Wolf <violetchandon@gmail.com>
2022-10-22 14:26:51 -0700

    Refactored code to use state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,12 +3,21 @@ import React, { useState } from 'react';
 import memesData from '../memesData';
 
 const Meme = () => {
-  const [memeImage, setMemeImage] = useState('');
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg',
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
 
   const getMemeImage = () => {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => {
+      return { ...prevMeme, randomImage: url };
+    });
   };
 
   return (
@@ -20,7 +29,7 @@ const Meme = () => {
           Get a new meme image
         </button>
       </div>
-      <img className='meme-image' src={memeImage} alt='' />
+      <img className='meme-image' src={meme.randomImage} alt='' />
     </main>
   );
 };
